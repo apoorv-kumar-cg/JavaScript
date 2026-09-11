02dbf7161b15a1cb5c53c6c0dd24775bc68af727
apoorv-kumar-cg <apoorvkumar.cg@gmail.com>
2026-09-11 15:04:04 +0530

    js added

---
+++ Arithmetic Operators assign.js
@@ -0,0 +1,116 @@
+// 1. Addition +
+
+// Q1
+let class1Collection = 15000;
+let class2Collection = 12500;
+let totalCollection = class1Collection + class2Collection;
+console.log(totalCollection);
+
+// Q2
+let morningPages = 18;
+let eveningPages = 25;
+let totalPagesRead = morningPages + eveningPages;
+console.log(totalPagesRead);
+
+// Q3
+let mondaySales = 125;
+let tuesdaySales = 178;
+let totalItemsSold = mondaySales + tuesdaySales;
+console.log(totalItemsSold);
+
+// 2. Subtraction -
+
+// Q1
+let totalSeats = 80;
+let occupiedSeats = 53;
+let emptySeats = totalSeats - occupiedSeats;
+console.log(emptySeats);
+
+// Q2
+let initialMarks = 500;
+let lostMarks = 35;
+let finalMarks = initialMarks - lostMarks;
+console.log(finalMarks);
+
+// Q3
+let totalBoxes = 2500;
+let boxesSent = 875;
+let remainingBoxes = totalBoxes - boxesSent;
+console.log(remainingBoxes);
+
+// 3. Multiplication *
+
+// Q1
+let costPerNotebook = 45;
+let numberOfNotebooks = 8;
+let totalCost = costPerNotebook * numberOfNotebooks;
+console.log(totalCost);
+
+// Q2
+let productionPerHour = 120;
+let hoursWorked = 6;
+let totalProduction = productionPerHour * hoursWorked;
+console.log(totalProduction);
+
+// Q3
+let numberOfRows = 7;
+let plantsPerRow = 15;
+let totalPlants = numberOfRows * plantsPerRow;
+console.log(totalPlants);
+
+// 4. Division /
+
+// Q1
+let totalPencils = 144;
+let totalStudents = 12;
+let pencilsPerStudent = totalPencils / totalStudents;
+console.log(pencilsPerStudent);
+
+// Q2
+let totalDistance = 360;
+let totalHours = 6;
+let averageDistancePerHour = totalDistance / totalHours;
+console.log(averageDistancePerHour);
+
+// Q3
+let totalAmount = 72000;
+let numberOfDepartments = 9;
+let amountPerDepartment = totalAmount / numberOfDepartments;
+console.log(amountPerDepartment);
+
+// 5. Modulus %
+
+// Q1
+let groupTotalStudents = 53;
+let groupSize = 5;
+let studentsLeftOver = groupTotalStudents % groupSize;
+console.log(studentsLeftOver);
+
+// Q2
+let totalCandies = 128;
+let candiesPerBox = 10;
+let candiesLeftUnpacked = totalCandies % candiesPerBox;
+console.log(candiesLeftUnpacked);
+
+// Q3
+let givenNumber = 17;
+let remainder = givenNumber % 2;
+console.log(remainder === 0);
+
+// 6. Exponentiation **
+
+// Q1
+let cubeSide = 6;
+let cubeVolume = cubeSide ** 3;
+console.log(cubeVolume);
+
+// Q2
+let initialBacteria = 1;
+let doublingHours = 4;
+let totalBacteria = initialBacteria * (2 ** doublingHours);
+console.log(totalBacteria);
+
+// Q3
+let sideCells = 9;
+let totalCells = sideCells ** 2;
+console.log(totalCells);
---
+++ index.js
@@ -0,0 +1,80 @@
+// function division() {
+//     let p = 100;
+//     let q = 20;
+//     return p / q;
+// }
+// console.log(division());
+
+
+
+// function demo() {
+//     console.log("This is a demo function");
+//     console.log("it's working fine");
+// }
+// demo();
+
+
+// function calculator(a, b) {
+//     console.log(a + b);
+//     console.log(a - b);
+//     console.log(a * b);
+//     console.log(a / b);
+// }
+// calculator(564, 9);
+
+
+// function name() {
+//     console.log("hello everyone");
+// }
+// name();
+
+// function name1() {
+//     return "hello everyone";
+// }
+// console.log(name1());
+
+// var a = "10";
+// var b = 20;
+
+// console.log(a + b);
+
+// let b = 10;
+// let b = 20;
+// console.log(b);
+
+// let c = 10;
+// c = 20;
+// console.log(c);
+
+
+// let a = 20
+// let b = 30
+// console.log(a + b);
+
+let rent = 100;
+let tax = rent * 0.18;
+let maintenance = rent * 0.05;
+let total = rent + tax + maintenance;
+console.log("Total monthly cost:","rs",total);
+
+
+let money = 1000;
+let expenses = 750;
+let remaining = money - expenses;
+console.log("Remaining money:","rs",remaining);
+console.log("MONEY")
+
+let totalTime = 200;
+let spendTime = 100;
+let remainingTime = totalTime-spendTime
+console.log("Remainingtime",remainingTime,"min")
+
+let productPrice = 15000;
+let productQuantity = 4;
+let finalPrice = productPrice*productQuantity
+console.log("Total price",finalPrice,"Rs")
+
+let bill = 900;
+let people = 3;
+let perPertion = bill/people;
+console.log(perPertion)
